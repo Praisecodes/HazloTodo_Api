8361d4f79b922ea6050c836cb38133fa052877e8
Praisecodes <cmancoder@gmail.com>
2022-09-22 00:50:02 +0100

    [Fix] Attempt fix duplication issue

--- signup.php
+++ signup.php
@@ -10,27 +10,32 @@
     $email = TestInput($decoded["email"]);
     $password = TestInput(md5($decoded["password"]));
 
-    $sql = "INSERT INTO user_info(fullname, username, email, user_password) VALUES(?,?,?,?);";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param('ssss', $fullname, $username, $email, $password);
-    if($stmt->execute()){
-        if(WelcomeEmail($email, $username)){
-            http_response_code(200);
-            $stmt->close();
-            $conn->close();
-            exit;
+    if(!empty($fullname) && !empty($username) && !empty($email) && !empty($password)){
+        $sql = "INSERT INTO user_info(fullname, username, email, user_password) VALUES(?,?,?,?);";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('ssss', $fullname, $username, $email, $password);
+        if($stmt->execute()){
+            if(WelcomeEmail($email, $username)){
+                http_response_code(200);
+                $stmt->close();
+                $conn->close();
+                exit;
+            }
+            else{
+                http_response_code(201);
+                $stmt->close();
+                $conn->close();
+                exit;
+            }
         }
         else{
-            http_response_code(201);
+            http_response_code(500);
             $stmt->close();
             $conn->close();
             exit;
         }
     }
     else{
-        http_response_code(500);
-        $stmt->close();
-        $conn->close();
         exit;
     }
 ?>
