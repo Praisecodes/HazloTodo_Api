a7b01b42df6d2e02162d454d26bde372bfeedfa1
Praisecodes <cmancoder@gmail.com>
2022-09-22 00:35:36 +0100

    Adding heroku connection

--- connection.php
+++ connection.php
@@ -1,13 +1,13 @@
 <?php
-    $conn = new mysqli('localhost', 'root', '', 'hazlodb');
+    // $conn = new mysqli('localhost', 'root', '', 'hazlodb');
 
-    // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    // $cleardb_server = $cleardb_url["host"];
-    // $cleardb_username = $cleardb_url["user"];
-    // $cleardb_password = $cleardb_url["pass"];
-    // $cleardb_db = substr($cleardb_url["path"],1);
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
 
-    // $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
     if($conn->connect_error){
         http_response_code(500);
