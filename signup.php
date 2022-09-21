375fa610f47e1caab58a20e088040cb0b2173104
Praisecodes <cmancoder@gmail.com>
2022-09-21 20:18:36 +0100

    [Update] Attempt fix on signup

--- signup.php
+++ signup.php
@@ -11,17 +11,18 @@
     $email = TestInput($decoded_contents["email"]);
     $password = TestInput(md5($decoded_contents["password"]));
 
-    $sql = "INSERT INTO `user_info` (fullname, username, email, user_password) VALUES(?,?,?,?);";
+    $sql = "INSERT INTO user_info (fullname, username, email, user_password) VALUES(?,?,?,?);";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param('ssss', $fullname, $username, $email, $password);
     if($stmt->execute()){
         if(WelcomeEmail($email, $username)){
             http_response_code(200);
+            exit;
         }
         else{
             http_response_code(201);
+            exit;
         }
-        exit;
     }
     else{
         http_response_code(500);
