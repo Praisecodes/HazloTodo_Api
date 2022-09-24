b3b3f1f004548c42a08ac66011c533ce4af8b64c
Praisecodes <cmancoder@gmail.com>
2022-09-24 16:10:57 +0100

    test

--- getactivitiescount.php
+++ getactivitiescount.php
@@ -11,7 +11,7 @@ use Firebase\JWT\Key;
     $skey = 'some_crazy_long_secret_key_I_used';
 
     if(true){
-        echo(json_encode($_SERVER['HTTP_AUTHORIZATION']));
+        // echo(json_encode($_SERVER['HTTP_AUTHORIZATION']));
         $decode = JWT::decode($_SERVER['HTTP_AUTHORIZATION'], new Key($skey, 'HS256'));
         // $decodearr = (array) $decode;
         // $user_id = $decodearr["id"];
